fb4f904dbe9004206f833c981b1871c8e1248bd3
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2025-03-29 12:19:00 +0530

    first commit

---
+++ src/main/java/org/sehn/multibranchpipelinedemo/MultibranchPipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package org.sehn.multibranchpipelinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultibranchPipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultibranchPipelineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sehn/multibranchpipelinedemo/MultibranchPipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sehn.multibranchpipelinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultibranchPipelineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
